7d341166eed9cb6baa69c6af9117bea1a65241d0
koceArigilshki <93599657+koceArigilshki@users.noreply.github.com>
2021-11-09 15:36:27 +0200

    Zad2
    Edit line 2

---
+++ ExpenseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BorrowLend.Models
+{
+    public class ExpenseType
+    {
+        int Id;
+        string ExpenseTypeName;
+    }
+}
